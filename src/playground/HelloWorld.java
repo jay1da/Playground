fd91b949bc5eb1436dd9c605ba57aeab06c9a3f6
adrian.francisco <adrian.francisco@canada.ca>
2017-03-01 16:37:53 +0000

    adding hello jay1da

---
+++ src/playground/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Name : HelloWorld.java
+ * Author : Adrian Francisco
+ * Created: Mar 1, 2017
+ */
+package playground;
+
+/**
+ * Hello World!
+ *
+ * @author Adrian Francisco
+ */
+public class HelloWorld {
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Jay1da!");
+    }
+}
